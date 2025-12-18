28797ee896e150dca12fdb105fc618353166b833
DuyMinhED <minhnd2424@ut.edu.vn>
2025-12-18 19:18:02 +0700

    Initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@utils': './src/utils',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@constants': './src/constants',
+          '@navigation': './src/navigation',
+        },
+      },
+    ],
+  ],
 };
